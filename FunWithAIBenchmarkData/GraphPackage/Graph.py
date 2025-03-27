58ccace7f73298baf5d7e80360e13a0704394ce3
bainesct <bainesct@VDI-LCB-IC-014>
2025-03-26 22:06:26 -0400

    Fixed documentation on graph.

--- FunWithAIBenchmarkData/GraphPackage/Graph.py
+++ FunWithAIBenchmarkData/GraphPackage/Graph.py
@@ -1,3 +1,18 @@
+# File Name : Image.py
+# Student Name: Collin Baines / Cole Crooks
+# email:  bainesct@mail.uc.edu / crookscl@mail.uc.edu
+# Assignment Number: Assignment 08
+# Due Date:   03/27/2025
+# Course #/Section:   IS4010-002
+# Semester/Year:   Spring/2025
+# Brief Description of the assignment:  This assignment has us display an image that is related to our team name, and create some sort of visulization
+
+# Brief Description of what this module does. This module creates a graph
+# Citations: ChatGPT
+
+# Anything else that's relevant:
+
+
 import tkinter as tk
 
 # Function to plot a line
